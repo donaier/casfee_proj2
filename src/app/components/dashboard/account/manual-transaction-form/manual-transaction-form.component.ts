1882a0b21cf3cc286642fc647ea08b920d8bbdfc
donat baier <donat.baier@gmail.com>
2022-12-04 17:13:33 +0100

    manual transactions form

--- src/app/components/dashboard/account/manual-transaction-form/manual-transaction-form.component.ts
+++ src/app/components/dashboard/account/manual-transaction-form/manual-transaction-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormGroupDirective } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { FluxStore } from 'src/app/shared/services/flux-store';
 import { Account } from 'src/app/shared/types/account';
+import { Category, CategoryGroup } from 'src/app/shared/types/category';
 import { TransactionForm } from 'src/app/shared/types/transaction';
 
 @Component({
@@ -8,20 +11,46 @@ import { TransactionForm } from 'src/app/shared/types/transaction';
   templateUrl: './manual-transaction-form.component.html',
   styleUrls: ['./manual-transaction-form.component.scss']
 })
-export class ManualTransactionFormComponent{
+export class ManualTransactionFormComponent implements OnInit {
 
   @Input() account?: Account;
   public transactionForm: FormGroup = new FormGroup(TransactionForm);
+  private categoryInput: HTMLInputElement | null = null
 
-  constructor() { }
+  private subscription : Subscription | undefined
+  categoryGroups: CategoryGroup[] = [];
+
+  constructor(public store: FluxStore) {}
+
+  ngOnInit(){
+    this.subscription = this.store.CategoryGroups.subscribe((data) => {
+      if (data.length > 0) {
+        this.categoryGroups = data;
+      }
+    })
+
+    this.categoryInput = document.querySelector('#category-manualTransaction')
+  }
 
   hideModal() {
     document.getElementById('manual-transaction-form')?.classList.remove('is-active');
     this.transactionForm.reset();
   }
 
-  submitTransactionForm(e: Event, form: FormGroupDirective) {
+  addCategory(category: CategoryGroup) {
 
   }
 
+  setCategory(e: Event, category: Category) {
+    if (this.categoryInput) {
+      this.categoryInput.value = category.name
+
+      document.querySelectorAll('.selectable-tag').forEach(tag => { tag.classList.remove('selected')});
+      (<HTMLElement>e.target).classList.add('selected')
+    }
+  }
+
+  submitTransactionForm(e: Event, form: FormGroupDirective) {
+
+  }
 }
