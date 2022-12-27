6dc3a9ac7f08ba0eb11919b38f4d59315a827eee
donat baier <donat.baier@gmail.com>
2022-12-27 16:41:33 +0100

    fix faulty manual transaction form date

--- src/app/components/dashboard/account/manual-transaction-form/manual-transaction-form.component.ts
+++ src/app/components/dashboard/account/manual-transaction-form/manual-transaction-form.component.ts
@@ -1,12 +1,13 @@
 import { Component, ElementRef, Inject, Input, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
+import * as moment from 'moment';
 import { Subject, Subscription } from 'rxjs';
 import { fluxDispatcherToken } from 'src/app/shared/helpers/flux.configuration';
 import { FluxStore } from 'src/app/shared/services/flux-store';
 import { Account, calculateCurrentValue } from 'src/app/shared/types/account';
 import { FluxAction, FluxActionTypes } from 'src/app/shared/types/actions.type';
 import { Category, CategoryGroup } from 'src/app/shared/types/category';
-import { Transaction } from 'src/app/shared/types/transaction';
+import { DATE_FORMAT, Transaction } from 'src/app/shared/types/transaction';
 
 @Component({
   selector: 'app-manual-transaction-form',
@@ -81,7 +82,9 @@ export class ManualTransactionFormComponent implements OnInit, OnDestroy {
 
     if(this.transactionForm.valid && this.transactionForm.dirty) {
       let account = Object.assign(this.account!)
-      account.transactions.push(this.transactionForm.value)
+      let transaction: Transaction = this.transactionForm.value
+      transaction.date = moment(this.transactionForm.get('date')?.value).format(DATE_FORMAT)
+      account.transactions.push(transaction)
       account.currentValue = Number(calculateCurrentValue(account))
 
       this.dispatcher.next(new FluxAction(FluxActionTypes.Update,'account', null, null, null, account))
