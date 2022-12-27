fea0db0ae36c7a097df586a27241b99b25b71c9c
kaayssmile <kaay.hertenstein@gmail.com>
2022-12-27 18:45:19 +0100

    add config for cypress

--- cypress/e2e/login/app.login.cy.ts
+++ cypress/e2e/login/app.login.cy.ts
@@ -8,7 +8,11 @@ describe('basic tests for todo app', () => {
     page.navigateTo();
   });
 
-  // Should contain all Elements.
+  describe('Some Test', () => {
+    it('Adds document to test_hello_world collection of Firestore', () => {
+      cy.callFirestore('add', 'test_hello_world', { some: 'value' });
+    });
+  });
 
   it('should render all elemets"', () => {
     page.getLoginSection().should('be.visible')
