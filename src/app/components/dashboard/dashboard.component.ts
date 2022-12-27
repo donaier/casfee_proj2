533e669455d559c41f877f963b12c6b9c98c4f04
donat baier <donat.baier@gmail.com>
2022-12-27 16:26:29 +0100

    accounts on dashboard toggleable

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { fluxDispatcherToken } from 'src/app/shared/helpers/flux.configuration';
+import { FluxStore } from 'src/app/shared/services/flux-store';
+import { Account } from 'src/app/shared/types/account';
+import { FluxAction, FluxActionTypes } from 'src/app/shared/types/actions.type';
 
 
 @Component({
@@ -6,7 +11,33 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  accounts: Account[] = []
+  activeAccounts: Account[] = []
 
-  constructor() {}
+  private subscription: Subscription[] = [];
+
+  constructor(@Inject(fluxDispatcherToken) private dispatcher: Subject<FluxAction>, public store: FluxStore){}
+
+  ngOnInit() {
+    this.dispatcher.next(new FluxAction(FluxActionTypes.Load))
+    this.subscription.push(this.store.Accounts.subscribe((data) => {
+      if (data.length > 0) {
+        this.accounts = data
+        this.activeAccounts = data
+      }
+    }))
+  }
+
+  toggleAccount(account: Account) {
+    if (this.activeAccounts.includes(account)) {
+      this.activeAccounts = this.activeAccounts.filter(a => a != account)
+    } else {
+      this.activeAccounts = [...this.activeAccounts, account]
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.forEach((subscription) => {subscription.unsubscribe()})
+  }
 }
