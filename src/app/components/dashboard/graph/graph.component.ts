4a57727e0ad89a44a993d598212db35d3d1ae0e7
donat baier <donat.baier@gmail.com>
2022-12-30 13:07:50 +0100

    chart setup

--- src/app/components/dashboard/graph/graph.component.ts
+++ src/app/components/dashboard/graph/graph.component.ts
@@ -1,13 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, Input, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { fluxDispatcherToken } from 'src/app/shared/helpers/flux.configuration';
+import { FluxStore } from 'src/app/shared/services/flux-store';
 import { Account } from 'src/app/shared/types/account';
+import { FluxAction } from 'src/app/shared/types/actions.type';
+import { Category, CategoryGroup } from 'src/app/shared/types/category';
+import { ListTransaction } from 'src/app/shared/types/transaction';
+
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.scss']
 })
-export class GraphComponent {
+export class GraphComponent implements OnInit, OnChanges, OnDestroy {
   @Input() accounts: Account[] = []
+  @Input() selectedTimes: string[] = []
 
-  constructor() { }
+  @ViewChild('graph') graphElement!: ElementRef
+
+  private subscriptions: Subscription[] = [];
+
+  allTransactions: ListTransaction[] = []
+  allCategories: Category[] = []
+  allCategoryGroups: CategoryGroup[] = []
+
+  graph: echarts.ECharts | null = null
+
+  activeMonths: Set<string> = new Set
+
+  constructor(
+    @Inject(fluxDispatcherToken) private dispatcher: Subject<FluxAction>,
+    public store: FluxStore
+  ) {}
+
+  ngOnInit() {
+    this.subscriptions.push(this.store.Categories.subscribe((data) => {
+      if (data.length) {
+        this.allCategories = data
+      }
+    }))
+    this.subscriptions.push(this.store.CategoryGroups.subscribe((data) => {
+      if (data.length) {
+        this.allCategoryGroups = data
+      }
+    }))
+
+    this.graph = echarts.init(this.graphElement.nativeElement);
+  }
+
+  ngOnChanges() {
+    let chartOptions = {}
+
+    this.graph?.setOption(chartOptions)
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => {subscription.unsubscribe()})
+  }
 }
