71c730aeee9cb1e57121f740efe73c89598d1304
donat baier <donat.baier@gmail.com>
2023-01-14 23:38:52 +0100

    tests for transaction service

--- cypress/e2e/unitTests/utility.service.cy.ts
+++ cypress/e2e/unitTests/utility.service.cy.ts
@@ -3,17 +3,16 @@ import { UtilityService } from 'src/app/shared/services/utility.service'
 import { Account } from 'src/app/shared/types/account'
 import { CategoryGroup, Category } from 'src/app/shared/types/category'
 
-
-describe('Unit Test Utility Service', function () {
+describe('Unit Test Utility Service', () => {
   const us = new UtilityService
 
   before(() => {
-    expect(us.checkavailableCategories, 'checkavailableCategories').to.be.a('function')
-    expect(us.calculateCurrentValue, 'calculateCurrentValue').to.be.a('function')
+    expect(us.checkavailableCategories).to.be.a('function')
+    expect(us.calculateCurrentValue).to.be.a('function')
   })
 
-  context('current Value Calculation', function() {
-    it('on Account with no transactions', function() {
+  context('current Value Calculation', () => {
+    it('on Account with no transactions', () => {
       cy.fixture('AccountNoTransactions').then((acc: Account) => {
         expect(us.calculateCurrentValue(acc)).to.eq(
           Number(acc.initialValue).toFixed(2)
@@ -21,7 +20,7 @@ describe('Unit Test Utility Service', function () {
       })
     })
 
-    it('on Account with a transaction', function() {
+    it('on Account with a transaction', () => {
       cy.fixture('AccountWithTransactions').then((acc: Account) => {
         expect(us.calculateCurrentValue(acc)).to.eq(
           Number(0).toFixed(2)
@@ -30,12 +29,12 @@ describe('Unit Test Utility Service', function () {
     })
   })
 
-  context('Category availability', function() {
-    it('without any data', function() {
+  context('Category availability', () => {
+    it('without any data', () => {
       expect(us.checkavailableCategories([], [])).to.be.empty
     })
     
-    it('with valid categories + groups', function() {
+    it('with valid categories + groups', () => {
       cy.fixture('ValidCategoriesAndGroups').then((cat: {groups: CategoryGroup[], categories: Category[]}) => {
         us.checkavailableCategories(cat.groups, cat.categories).forEach(group => {
           if (group.name == 'hasNoChildren') {
--- src/app/shared/services/transaction.service.ts
+++ src/app/shared/services/transaction.service.ts
@@ -81,7 +81,7 @@ export class TransactionService {
 
     uniqueYears.forEach(y => groupedMonths.push(uniqueMonths.filter(m => m.includes(y))))
 
-    return groupedMonths
+    return groupedMonths.flat()
   }
 
 }
